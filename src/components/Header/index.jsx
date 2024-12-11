39e18dc36875ce0cbeb0d9e4776f9ad081548ddb
vlecabr <vitartasleca@gmail.com>
2024-12-11 16:45:27 -0300

    header adicionado, colocar na exibição

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from '../../assets/logo.png';
+
+function Header() {
+    return (
+        <header>
+        <Logo src={logo} alt="Fastask Logo" />
+        </header>
+    );
+}
