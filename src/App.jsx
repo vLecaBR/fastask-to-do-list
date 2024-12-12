589ef23694c8a9ee2a7fb84c69bd87346d57526a
vlecabr <vitartasleca@gmail.com>
2024-12-12 14:41:39 -0300

    arrumando para funcionar sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 // App.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from './components/Sidebar';
 import TaskList from './components/TaskList';
 import { Container, HeaderContainer } from './App.styles';
 import Header from './components/Header';
 
 function App() {
+  const [currentSection, setCurrentSection] = useState('Hoje'); // Estado para controlar a seção atual
+
   return (
     <div>
       <HeaderContainer>
-      <Header />
+        <Header />
       </HeaderContainer>
-    <Container>
-      <Sidebar />
-      <TaskList />
-    </Container>
+      <Container>
+        <Sidebar setCurrentSection={setCurrentSection} />
+        <TaskList section={currentSection} />
+      </Container>
     </div>
   );
 }
