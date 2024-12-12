f597f1632f87928c84f370b1384e2cbc010ac712
vlecabr <vitartasleca@gmail.com>
2024-12-12 09:27:53 -0300

    ajustado o avatar

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -1,25 +1,25 @@
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.div`
-    width: 100%;
-    background-color: #ffffff;
-    padding: 20px;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-    display: flex;
-    justify-content: space-between; /* Divide o espaço entre a logo e a seção direita */
-    align-items: center;
+  width: 100%; /* O header ocupa toda a largura da tela */
+  background-color: #ffffff;
+  padding: 20px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: space-between; /* Espaça os elementos: logo à esquerda, right section à direita */
+  align-items: center; /* Alinha verticalmente os itens no centro do header */
+  position: relative;
 `;
 
 export const Logo = styled.img`
-    width: 150px;
-    margin: 0; /* Remove margens desnecessárias */
-    margin-left: 40px; /* Pequeno espaçamento à esquerda */
+  width: 150px; /* Ajusta o tamanho da logo */
+  margin-left: 20px; /* Pequeno espaçamento da borda esquerda */
 `;
 
 export const RightSection = styled.div`
-    display: flex;
-    align-items: center; /* Alinha os itens no centro verticalmente */
-    justify-content: flex-end; /* Mantém os itens alinhados à direita */
-    gap: 20px; /* Adiciona espaçamento uniforme entre os itens */
-    margin-right: 40px; /* Pequeno espaçamento à direita do header */
+  display: flex;
+  align-items: center; /* Alinha verticalmente os itens */
+  justify-content: flex-end; /* Garante que itens internos fiquem à direita */
+  gap: 20px; /* Espaçamento entre os itens */
+  margin-left: 95vw; /* Pequeno espaçamento da borda direita */
 `;
