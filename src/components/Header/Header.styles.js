216ae41c505c32d781e0f40a0a28c36cbb6c8c92
vlecabr <vitartasleca@gmail.com>
2024-12-13 10:20:17 -0300

    header ajustado

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -3,23 +3,22 @@ import styled from 'styled-components';
 export const HeaderContainer = styled.div`
   width: 100%; /* O header ocupa toda a largura da tela */
   background-color: #ffffff;
-  padding: 20px;
+  padding: 10px 20px; /* Ajusta o espaçamento interno */
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
   display: flex;
-  justify-content: space-between; /* Espaça os elementos: logo à esquerda, right section à direita */
+  justify-content: space-between; /* Espaça os elementos: logo à esquerda, avatar à direita */
   align-items: center; /* Alinha verticalmente os itens no centro do header */
-  position: relative;
+  margin-top: 15px;
 `;
 
 export const Logo = styled.img`
   width: 150px; /* Ajusta o tamanho da logo */
-  margin-left: 20px; /* Pequeno espaçamento da borda esquerda */
-  margin-top: 20px; /* Pequeno espaçamento da borda superior */
+  margin-left: 29px; /* Ajusta o espaçamento à esquerda */
 `;
 
 export const RightSection = styled.div`
   display: flex;
   align-items: center; /* Alinha verticalmente os itens */
-  justify-content: flex-end; /* Garante que itens internos fiquem à direita */
-  margin-left: 95vw; /* Pequeno espaçamento da borda direita */
+  justify-content: center; /* Não há necessidade de espaçamento extra */
+  margin-right: 50px; /* Ajusta o espaçamento à direita */
 `;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,16 @@
-import React from "react";
+import React from 'react';
 import logo from '../../assets/logo.png';
-import { Logo, RightSection } from './Header.styles';
+import { HeaderContainer, Logo, RightSection } from './Header.styles';
 import { Avatar } from '@mui/material';
-
 
 function Header() {
     return (
-        <header>
-        <Logo src={logo} alt="Fastask Logo" />
-        <RightSection>
-            <Avatar alt="Usuário" src="https://github.com/vLecaBR.png" />
-        </RightSection>
-        </header>
+        <HeaderContainer>
+            <Logo src={logo} alt="Fastask Logo" />
+            <RightSection>
+                <Avatar alt="Usuário" src="https://github.com/vLecaBR.png" />
+            </RightSection>
+        </HeaderContainer>
     );
 }
 
