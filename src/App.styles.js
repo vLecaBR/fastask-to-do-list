cbb896b8d07431516f7424f5f71b9496f16e40d2
vlecabr <vitartasleca@gmail.com>
2024-12-17 09:02:34 -0300

    apagando estilos nao usados

--- src/App.styles.js
+++ src/App.styles.js
@@ -8,14 +8,3 @@ export const Container = styled.div`
   background-color: #f7f7f7;
   color: #333;
 `;
-
-// export const HeaderContainer = styled.div`
-//   width: 100%;
-//   background-color: #ffffff;
-//   padding: 20px;
-//   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-//   position: relative;
-// `;
