5e8710a7b91d02f79f0250e894fc1249101fded3
vlecabr <vitartasleca@gmail.com>
2024-12-26 12:10:27 -0300

    ajustando estilos para o darkmode

--- src/App.styles.js
+++ src/App.styles.js
@@ -6,6 +6,6 @@ export const Container = styled.div`
   height: 100vh;
   font-family: Arial, sans-serif;
   background-color: ${({ isDarkMode }) => (isDarkMode ? '#333' : '#f7f7f7')};
-  color: ${({ isDarkMode }) => (isDarkMode ? '#f7f7f7' : '#333')};
+  color: ${({ isDarkMode }) => (isDarkMode ? '#fff' : '#333')};
   transition: background-color 0.3s, color 0.3s;
 `;
