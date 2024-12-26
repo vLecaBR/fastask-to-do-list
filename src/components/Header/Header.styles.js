7a7ad34ab7c5e18148c04ddcdc76b35b753d007e
vlecabr <vitartasleca@gmail.com>
2024-12-26 12:39:00 -0300

    front "finalizado" partir para autenticação e alocação dos dados

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -4,7 +4,7 @@ export const HeaderContainer = styled.div`
   width: 100%;
   background-color: ${({ isDarkMode }) => (isDarkMode ? '#444' : '#ffffff')};
   color: ${({ isDarkMode }) => (isDarkMode ? '#f7f7f7' : '#333')};
-  padding: 10px 20px;
+  padding: 10px 0px;
   box-shadow: ${({ isDarkMode }) =>
     isDarkMode ? '0 2px 5px rgba(255, 255, 255, 0.1)' : '0 2px 5px rgba(0, 0, 0, 0.1)'};
   display: flex;
@@ -15,6 +15,7 @@ export const HeaderContainer = styled.div`
 
 export const Logo = styled.img`
   width: 150px;
+  padding-left: 45px;
 `;
 
 export const RightSection = styled.div`
